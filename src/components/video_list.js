8be112efe93ef54920d578772b2d817184ce98ab
Hamza Elmiqdam <Elmiqdam@gmail.com>
2017-01-09 03:18:20 +0000

    first push
    Tutorial udemy-Modern React With Redux video 29/66

---
+++ src/components/video_list.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import VideosListItem from './video_list_item';
+
+const VideosList = (props) => {
+  const videoItems = props.videos.map((video)=>{
+    return (
+      <VideosListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video}/>
+    );
+  });
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+}
+export default VideosList;
